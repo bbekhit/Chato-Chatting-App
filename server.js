8944b43e9ff8552b38355546b1034729ed9831f4
boutros <boutrossamir78@gmail.com>
2018-07-07 01:28:46 -0700

    server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
-const path = require('path');
+const path = require("path");
 
 const users = require("./routes/api/users");
 const posts = require("./routes/api/posts");
@@ -10,16 +10,15 @@ const comments = require("./routes/api/comments");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({
+app.use(
+  bodyParser.urlencoded({
     extended: false
-}));
+  })
+);
 app.use(bodyParser.json());
-
-app.get("/", (req, res) => res.send("Hello Chato"));
 
 const db = require("./config/keys").mongoURI;
 mongoose.connect(db).then(() => console.log("MongoDB is connected"));
-
 
 // passport middleware
 app.use(passport.initialize());
@@ -27,23 +26,19 @@ app.use(passport.initialize());
 // passport config
 require("./config/passport")(passport);
 
-
 app.use("/api/users", users);
 app.use("/api/posts", posts);
 app.use("/api/comments", comments);
 
-
-
 // Server static assets if in production
-if (process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static('client/build'));
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
-
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`chato running on port ${port}`));
